69ed7d274a898ead00e68de44160b89b327c60b0
James Haver <mchaver@gmail.com>
2017-06-29 11:21:06 +0800

    Outputting correctly formatted pdfs

--- src/Lib.hs
+++ src/Lib.hs
@@ -24,7 +24,8 @@ utarticle = "utarticle"
 
 thePreamble :: Monad m => LaTeXT m ()
 thePreamble = do 
-  documentclass [CustomOption "a4j", CustomOption "12pt", landscape] utarticle
+  documentclass [CustomOption "12pt", CustomOption "a4j", landscape, CustomOption "dvipdfmx"] utarticle
+  usepackage [] "color"  
   usepackage [] "hhline"
   suppressPageNumbering
   
@@ -76,8 +77,8 @@ matrixTabular ts m =
 -- lnbk  :: LaTeXC l => l
 -- lnbk = fromLaTeX $ TeXLineBreak Nothing False
 
-tnln :: LaTeXC l => l
-tnln = comm0 "tabularnewline"
+tabularnewline :: LaTeXC l => l
+tabularnewline = comm0 "tabularnewline"
 
 --textbf :: LaTeXC l => l -> l
 --textbf = liftL $ \l -> TeXComm "textbf" [FixArg l]
@@ -85,31 +86,66 @@ tnln = comm0 "tabularnewline"
 hhline :: LaTeXC l => l -> l
 hhline = liftL $ \l -> TeXComm "hhline" [FixArg l]
 
+-- \hhline{|t:=:t:=:t:=:t|}
+hhlineTop :: LaTeXC l => Int -> l
+hhlineTop x = hhline (mconcat ["|", xs, end])
+  where
+    xs = mconcat $ replicate x "t:=:"
+    end = if x > 0 then "t|" else "|"
+
+-- \hhline{|:=::=::=:|}
 hhlineMiddle :: LaTeXC l => Int -> l
 hhlineMiddle x = hhline (mconcat ["|", xs, "|"])
   where
     xs = mconcat $ replicate x ":=:"
--- \hhline{|:=::=::=:|}
 
-cell :: LaTeXC l => Text -> Text -> Text -> l
-cell a b c = ((raw a) & (raw b) & (raw c))
+-- \hhline{|b:=:b:=:b:=:b|}
+hhlineBottom :: LaTeXC l => Int -> l
+hhlineBottom x = hhline (mconcat ["|", xs, end])
+  where
+    xs = mconcat $ replicate x "b:=:"
+    end = if x > 0 then "b|" else "|"
 
+{-
+--\def\cell(#1,#2,#3){
+--  \rule{0pt}{3ex} \hspace*{-.4cm}  {\LARGE #1} \newline
+--  \rule{0pt}{3ex} \hspace*{.4cm}   {\small #2} \newline
+--  \rule{0pt}{3ex} \hspace*{.425cm} {\small #3}
+--}
+-}
+
+cell :: Monad m => Text -> Text -> Text -> LaTeXT m ()
+cell t1 t2 t3 = do
+  fromLaTeX $ TeXComm "rule" [FixArg "0pt", FixArg "3ex"]
+  comm1 "hspace*" "-.4cm"
+  large3 $ raw t1
+  newline
+  
+  fromLaTeX $ TeXComm "rule" [FixArg "0pt", FixArg "3ex"]
+  comm1 "hspace*" ".4cm"
+  small $ raw t2
+  newline
+  
+  fromLaTeX $ TeXComm "rule" [FixArg "0pt", FixArg "3ex"]
+  comm1 "hspace*" ".425cm"
+  small $ raw t3
+
+-- \begin{tabular}{||p{5.5cm} || p{5.5cm} || p{5.5cm} ||}
+{-
+\cell(おとおさん,お父さん,father) & \cell(おかあさん,お母さん,mother) & \cell(おにいさん,お兄さん,younger brother)
+\tnewline
+
+\cell(おじさん,叔父さん,uncle) & \cell(たのしい,楽しい,fun) & \cell(かえる,帰る,go home)
+\tnewline
+-}
 theBody :: Monad m => LaTeXT m ()
 theBody = do
-  tabular Nothing [VerticalLine, CenterColumn, VerticalLine, CenterColumn, VerticalLine] 
-    (mconcat [noindent, (cell "Hello" "Goodbye" "Goodnight"), tnln, hhlineMiddle 3])
-  -- Table from a simple matrix
-  center $ matrixTabular (fmap textbf ["x","y","z"]) $
-    fromList 3 3 [ (1 :: Int)..]
-  -- Table from a matrix calculated in-place
-  center $ matrixTabular (fmap textbf ["Number","Square root"]) $
-    matrix 9 2 $ \(i,j) -> if j == 1 then I i else R $ sqrt $ fromIntegral i
-
--- Creating custom instances of Texy to display elements
--- within a table.
-
-data Number = R Double | I Int
-
-instance Texy Number where
-  texy (R x) = texy x
-  texy (I i) = texy i
+  noindent
+  tabular Nothing [DVerticalLine, ParColumnTop "5.5cm", DVerticalLine, ParColumnTop "5.5cm", DVerticalLine, ParColumnTop "5.5cm", DVerticalLine] 
+    (mconcat 
+      [ hhlineTop 3
+      , ((cell "おとおさん" "お父さん" "father") & (cell "おかあさん" "お母さん" "mother") & (cell "おにいさん" "お兄さん" "younger brother")), tabularnewline, hhlineMiddle 3
+      , ((cell "おとおさん" "お父さん" "father") & (cell "おかあさん" "お母さん" "mother") & (cell "おにいさん" "お兄さん" "younger brother")), tabularnewline
+      , hhlineBottom 3
+      ]
+    )
